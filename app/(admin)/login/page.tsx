ffb523645afbb3c171359cc15a9bb0c817fb49e5
mohamedElmesery999 <mohamedelmesery18@gmail.com>
2026-07-06 17:05:26 +0300

    Update page.tsx

--- app/(admin)/login/page.tsx
+++ app/(admin)/login/page.tsx
@@ -70,7 +70,7 @@ export default function Login() {
               id="email"
               label="البريد الإلكتروني"
               type="email"
-              placeholder="hooks@gmail.com"
+              placeholder="example@gmail.com"
               dir="ltr"
               className="text-left"
               error={errors.email?.message}
